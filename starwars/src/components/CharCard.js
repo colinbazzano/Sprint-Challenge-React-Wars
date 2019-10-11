614748f8946ff04da850f906dc5c97e30657aabb
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-11 11:58:30 -0400

    made changes to the Char Card to add labeling to each property grabbed by the API

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -3,10 +3,10 @@ import React from 'react';
 const CharCard = props => {
     return (
         <div className='charCard' key={props.index}>
-            <h2>{props.name}</h2>
-            <p>{props.birth_year}</p>
-            <p>{props.height}</p>
-            <p>{props.skin_color}</p>
+            <h2>Name: {props.name}</h2>
+            <p>Birth Year: {props.birth_year}</p>
+            <p>Height: {props.height}</p>
+            <p>Skin Color: {props.skin_color}</p>
         </div>
     );
 };
